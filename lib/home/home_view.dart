42a0f35ff3b6fa48ba8464ff36b50233eaf16050
Rohit Joshi <rohitj782@gmail.com>
2022-03-23 21:34:13 +0530

    Added Home page.

---
+++ lib/home/home_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/profile/profile_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children:  const [
+            ProfileView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
